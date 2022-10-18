1b10a98a3145c8aa9cc73d74b6cc988fe8498437
Moises <ve11621@innovaccion.mx>
2022-10-18 11:22:02 -0500

    Register user component done, also I implemented the functionality with a simple modal, so I save time by doing another view just for two simple inputs

--- src/components/DashboardMainComponent.vue
+++ src/components/DashboardMainComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { getCurrentInstance, ref, type ComponentOptionsBase, type ComponentPublicInstance } from "vue";
+import { getCurrentInstance, provide, ref, type ComponentOptionsBase, type ComponentPublicInstance, type Ref } from "vue";
 import NavBarComponent from "./NavBarComponent.vue";
-
+import ModalCreateNewUser from "./ModalCreateNewUser.vue";
 
 interface usuarioAtributtes{
     id:string,
@@ -9,11 +9,8 @@ interface usuarioAtributtes{
     edad?:string
 }
 
-let updateAfterDelete=ref(0);
+let updateAfterRegistryDone=ref(0) as Ref<number>;
 let xmlResponsebject:Array<usuarioAtributtes> = [];
-
-//const userRowToBeRemoved=ref<Element | ComponentPublicInstance | null >(null);
-/*console.log(ejemploComponente);*/
 
 function obtenerFetchInicial(){
     let bringAllUsuarios = new XMLHttpRequest();
@@ -24,10 +21,8 @@ function obtenerFetchInicial(){
     bringAllUsuarios.onreadystatechange = () => {
         if (bringAllUsuarios.readyState === XMLHttpRequest.DONE) {
             if (bringAllUsuarios.status === 200) {
-                //console.log(bringAllUsuarios.responseXML);
                 /*Snippet taken from https://stackoverflow.com/questions/27438590/extract-value-from-xml-using-pure-javascript */        
                 var XMLListIterator = ((((bringAllUsuarios.responseText) as string).match(/<ns2:usuariosLista>(.*?)<\/ns2:usuariosLista>/g)) as RegExpMatchArray);
-
                 if(XMLListIterator!==undefined || XMLListIterator!==null){
                     XMLListIterator.forEach((value, id) => {
                         let parsedXMLListElement=XMLListIterator[id].replace(/(<([^>]+)>)/ig, ",").replace(",,","").split(",,");
@@ -62,7 +57,6 @@ function obtenerFetchInicial(){
 }
 
 function deleteUsuario(id:number,prueba:any):void{ 
-    
     let deleteXmlRequest=new XMLHttpRequest();
     deleteXmlRequest.open('POST','http://localhost:8080/ws/usuario.wdsl',true);
     deleteXmlRequest.setRequestHeader('Content-type','text/xml');
@@ -92,14 +86,23 @@ function deleteUsuario(id:number,prueba:any):void{
     </Envelope>`);
 }
 
+function openRegistryModal(){
+    updateAfterRegistryDone.value=1;
+}
+
 obtenerFetchInicial();
+provide('updateAfterRegistryDone',updateAfterRegistryDone);
 </script>
 
 <template>
     <NavBarComponent />
-    <header ref="ejemploComponente" class="h3 text-center">SOAP Based crud with spring boot and vue</header>
+    <ModalCreateNewUser v-if="updateAfterRegistryDone===1" :key="updateAfterRegistryDone"/>
+    <section class="d-flex justify-content-around">
+        <header class="h3 text-center">SOAP Based crud with spring boot and vue</header>
+        <button @click="openRegistryModal()" class="btn btn-primary">Crear nuevo usuario</button>
+    </section>
     <section class="d-flex justify-content-center align-items-start min-vh-100 fondo-negro">
-        <table class="table" :key="updateAfterDelete">
+        <table class="table">
             <thead>
                 <tr>
                     <th scope="col">ID</th>
@@ -113,13 +116,13 @@ obtenerFetchInicial();
                     <th scope="row">{{item.id}}</th>
                     <td>{{item.nombreCompleto}}</td>
                     <td>{{item.edad}}</td>
-                    <td><button class="btn btn-info">Modify</button>
-                        <button @click="deleteUsuario(+item.id,$event)"  class="btn btn-danger">Delete</button></td>
+                    <td><button class="btn btn-info">Modificar</button>
+                        <button @click="deleteUsuario(+item.id,$event)"  class="btn btn-danger">Borrar</button></td>
                 </tr>
             </tbody>
         </table>
     </section>
-</template>
-<style scoped>
 
-</style>
+
+</template>
+<style scoped></style>
---
+++ src/components/ModalCreateNewUser.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { inject, ref, type Ref } from 'vue';
+
+let updateAfterRegistryDoneInstance=inject('updateAfterRegistryDone') as Ref<number>;
+let nombreCompletoRef=ref('');
+let edadRef=ref(0);
+
+function closeModalAfterRegistry(){
+    updateAfterRegistryDoneInstance.value=0;
+}
+
+function registerNewUser(){
+    let registerUser = new XMLHttpRequest();
+    console.log(nombreCompletoRef.value,edadRef.value);
+    registerUser.open("POST", "http://localhost:8080/ws/usuario.wsdl", false);
+    registerUser.setRequestHeader("Content-type", "text/xml");
+    registerUser.setRequestHeader("SOAPAction", "http://localhost:8080/soap/save-user");
+
+    registerUser.onreadystatechange=()=>{
+        if(registerUser.readyState===XMLHttpRequest.DONE){
+            if(registerUser.status===200){
+                console.log(registerUser.responseXML);
+                alert('USUARIO REGISTRADO EXITOSAMENTE');
+            }else{
+                alert(registerUser.statusText+' '+registerUser.status);
+            }
+        }
+    }
+
+    registerUser.send(`
+    <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+        <Body>
+            <saveUsuariosRequest xmlns="http://webspring.io/gs-prod">
+                <name>
+                    <id>[long]</id>
+                    <nombreCompleto>${nombreCompletoRef.value}</nombreCompleto>
+                    <edad>${edadRef.value}</edad>
+                </name>
+            </saveUsuariosRequest>
+        </Body>
+    </Envelope>
+    `);
+}
+</script>
+<template>
+<section class="d-flex justify-content-center align-items-center min-vh-100 modal-register">
+    <section class="card w-50 h-auto">
+        <section class="card-body">
+            <aside class="d-flex justify-content-center w-100">
+                <header class="ms-auto h3 text-center">Registrar nuevo usuario</header>
+                <button @click="closeModalAfterRegistry()" class="ms-auto btn-close"></button>
+            </aside>
+            <div class="mb-3">
+                <label class="form-label" for="nombreCompletoInput">Nombre completo</label>
+                <input v-model="nombreCompletoRef" type="text" id="nombreCompletoInput" class="form-control" placeholder="Ingrese nombre completo">
+            </div>
+            <div class="mb-3">
+                <div class="mb-3">
+                    <label for="edadInput" class="form-label">Edad</label>
+                    <input v-model="edadRef" type="number" min="0" max="200" id="edadInput" class="form-control">
+                </div>
+            </div>
+            <div class="mb-3">
+                <button @click="registerNewUser()" class="btn btn-primary w-100">
+                    Registrar usuario
+                </button>
+            </div>
+        </section>
+    </section>
+</section>
+</template>
+<style scoped>
+.modal-register{
+    width: 100%;
+    height: 100%;
+    background-color: #00000033;
+    position: fixed;
+    z-index: 999999999;
+}
+</style>
